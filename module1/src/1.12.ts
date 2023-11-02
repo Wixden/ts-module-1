6f21f4bc43defe9810573d7853c60dd6646859f0
Wixden <watchonlytutorial@gmail.com>
2023-11-03 03:05:57 +0600

    Chapter 12 Complete

---
+++ module1/src/1.12.ts
@@ -0,0 +1,40 @@
+{
+  // Nullable types + Unknown types
+
+  // Nullable types
+  const searchName = (value: string | null) => {
+    if (value) {
+      console.log("Searching");
+    } else {
+      console.log("There is nothing to search");
+    }
+  };
+  searchName(null);
+
+  // Unknown types
+  const getSpeedInMPS = (value: unknown) => {
+    if (typeof value === "number") {
+      const convertSpeed = (value * 1000) / 3600;
+      console.log(`The speed is ${convertSpeed}`);
+    } else if (typeof value === "string") {
+      const [valueNumber, text] = value.split(" ");
+      const convertSpeed = (parseFloat(valueNumber) * 1000) / 3600;
+      console.log(`The speed is ${convertSpeed}`);
+    } else {
+      console.log("Wrong value");
+    }
+  };
+
+  // getSpeedInMPS(100);
+  getSpeedInMPS(null);
+
+  // Never
+  // When something will never return anything is called never types
+  const throwError = (msg: string): never => {
+    throw new Error(msg);
+  };
+
+  throwError("Something went wrong");
+
+  //
+}
